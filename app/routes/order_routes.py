64c0c061206a400787e87d72b832c890e9b58549
GhouI <flareonverm@outlook.com>
2024-01-02 23:45:21 +0000

    Create order_routes.py

---
+++ app/routes/order_routes.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, jsonify, request
+from .services import order_service
+
+bp = Blueprint('orders', __name__, url_prefix='/orders')
+
+@bp.route('/', methods=['GET'])
+def get_orders():
+    orders = order_service.get_all_orders()
+    return jsonify([order.to_dict() for order in orders])
+
+@bp.route('/', methods=['POST'])
+def create_order():
+    data = request.get_json()
+    order = order_service.create_order(data['product_name'], data['customer_name'], data['quantity'])
+    return jsonify(order.to_dict()), 201
