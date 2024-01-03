6555c9fe7eee716a2ccf6e5d14f52457cccc8b14
GhouI <flareonverm@outlook.com>
2024-01-03 22:42:17 +0000

    Create customer_routes.py

---
+++ app/routes/customer_routes.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, jsonify, request
+from .services import customer_service
+
+bp = Blueprint('customers', __name__, url_prefix='/customers')
+
+@bp.route('/', methods=['GET'])
+def get_customers():
+    customers = customer_service.get_all_customers()
+    return jsonify([customer.to_dict() for customer in customers])
+
+@bp.route('/', methods=['POST'])
+def create_customer():
+    data = request.get_json()
+    customer = customer_service.create_customer(data['name'], data['email'])
+    return jsonify(customer.to_dict()), 201
