4fc4d6226c5e58bf7a084c53d866b7caaec2446e
GhouI <flareonverm@outlook.com>
2024-01-03 22:41:26 +0000

    Create customer.py

---
+++ app/models/customer.py
@@ -0,0 +1,15 @@
+from . import db 
+
+class Customer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128))
+    email = db.Column(db.String(128))
+    orders = db.relationship('Order', backref='customer', lazy=True)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'email': self.email,
+            'orders': [order.to_dict() for order in self.orders]
+        }
