086ec567cd9ff91707dbf0330b119652c3358b00
GhouI <flareonverm@outlook.com>
2024-01-04 22:02:40 +0000

    Update product_routes.py

--- app/routes/product_routes.py
+++ app/routes/product_routes.py
@@ -1,15 +1,16 @@
 from flask import Blueprint, jsonify, request
 from .services import product_service
+from bson import json_util
 
 bp = Blueprint('products', __name__, url_prefix='/products')
 
 @bp.route('/', methods=['GET'])
 def get_products():
     products = product_service.get_all_products()
-    return jsonify([product.to_dict() for product in products])
+    return jsonify(json_util.dumps([product.to_dict() for product in products]))
 
 @bp.route('/', methods=['POST'])
 def create_product():
     data = request.get_json()
     product = product_service.create_product(data['name'], data['description'], data['price'])
-    return jsonify(product.to_dict()), 201
+    return jsonify(json_util.dumps(product.to_dict())), 201
