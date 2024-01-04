79abfc466714f15516a344953292b838a30a95d7
GhouI <flareonverm@outlook.com>
2024-01-04 22:02:26 +0000

    Update customer_routes.py

--- app/routes/customer_routes.py
+++ app/routes/customer_routes.py
@@ -1,15 +1,16 @@
 from flask import Blueprint, jsonify, request
 from .services import customer_service
+from bson import json_util
 
 bp = Blueprint('customers', __name__, url_prefix='/customers')
 
 @bp.route('/', methods=['GET'])
 def get_customers():
     customers = customer_service.get_all_customers()
-    return jsonify([customer.to_dict() for customer in customers])
+    return jsonify(json_util.dumps([customer.to_dict() for customer in customers]))
 
 @bp.route('/', methods=['POST'])
 def create_customer():
     data = request.get_json()
     customer = customer_service.create_customer(data['name'], data['email'])
-    return jsonify(customer.to_dict()), 201
+    return jsonify(json_util.dumps(customer.to_dict())), 201
