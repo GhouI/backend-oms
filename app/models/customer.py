532eb70ff00af32e606d63116f921601747a2185
GhouI <flareonverm@outlook.com>
2024-01-04 21:59:33 +0000

    Update customer.py

--- app/models/customer.py
+++ app/models/customer.py
@@ -1,15 +1,26 @@
-from . import db 
+from bson.objectid import ObjectId
+from . import mongo
 
-class Customer(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128))
-    email = db.Column(db.String(128))
-    orders = db.relationship('Order', backref='customer', lazy=True)
+class Customer:
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
 
-    def to_dict(self):
+    @staticmethod
+    def to_dict(customer):
         return {
-            'id': self.id,
-            'name': self.name,
-            'email': self.email,
-            'orders': [order.to_dict() for order in self.orders]
+            'id': str(customer['_id']),
+            'name': customer['name'],
+            'email': customer['email'],
+            'orders': [str(order) for order in customer['orders']]
         }
+
+    @staticmethod
+    def from_db(customer_id):
+        customer = mongo.db.customers.find_one({'_id': ObjectId(customer_id)})
+        return Customer.to_dict(customer) if customer else None
+
+    def save_to_db(self):
+        customer = {'name': self.name, 'email': self.email, 'orders': []}
+        result = mongo.db.customers.insert_one(customer)
+        return str(result.inserted_id)
