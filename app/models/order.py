c869e53079c9387c31a6a80331096662103b6c13
GhouI <flareonverm@outlook.com>
2024-01-04 22:00:17 +0000

    Update order.py

--- app/models/order.py
+++ app/models/order.py
@@ -1,7 +1,27 @@
-from . import db
+from bson.objectid import ObjectId
+from . import mongo
 
-class Order(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    product_name = db.Column(db.String(128))
-    customer_name = db.Column(db.String(128))
-    quantity = db.Column(db.Integer)
+class Order:
+    def __init__(self, product_name, customer_name, quantity):
+        self.product_name = product_name
+        self.customer_name = customer_name
+        self.quantity = quantity
+
+    @staticmethod
+    def to_dict(order):
+        return {
+            'id': str(order['_id']),
+            'product_name': order['product_name'],
+            'customer_name': order['customer_name'],
+            'quantity': order['quantity']
+        }
+
+    @staticmethod
+    def from_db(order_id):
+        order = mongo.db.orders.find_one({'_id': ObjectId(order_id)})
+        return Order.to_dict(order) if order else None
+
+    def save_to_db(self):
+        order = {'product_name': self.product_name, 'customer_name': self.customer_name, 'quantity': self.quantity}
+        result = mongo.db.orders.insert_one(order)
+        return str(result.inserted_id)
